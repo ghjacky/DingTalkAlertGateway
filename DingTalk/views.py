d9ca7e7bac7033bb9ddce338082262437e4736bf
ghjacky <ldcrgmy@gmail.com>
2018-01-22 11:34:46 +0800

    init

--- DingTalk/views.py
+++ DingTalk/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.views import View
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -10,3 +10,4 @@ class AlertGateway(View):
     def post(self, request):
         print(request.POST.get('version', "empty"))
         print(request.POST.get('alerts', "empty"))
+        return HttpResponse(request)
